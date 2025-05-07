4f573dae913a93768bfc372f980fbc02b4c3b3cb
I-am-a-Bird <krazydyo@gmail.com>
2025-05-08 01:35:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello GIT!"<<endl; 
-    cout<<"ioy";
-    cout<<"we are making some changes..."
+       cout<<"Hello GIT!"<<endl; 
+    
+    cout<<"we are making some changes...";
+    cout<<"newBranch";
+    cout<<"old letter";
     return 0;
 }
