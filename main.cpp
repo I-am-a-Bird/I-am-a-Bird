1f67c033ac739630807737b3edf7dcdb4b706a83
I-am-a-Bird <krazydyo@gmail.com>
2025-05-08 00:57:16 +0300

    delete ioy

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     cout<<"Hello GIT!"<<endl; 
-    cout<<"ioy";
+    
     cout<<"we are making some changes...";
     cout<<"newBranch";
     cout<<"old letter";
