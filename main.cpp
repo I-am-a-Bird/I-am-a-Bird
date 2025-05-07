013fb1e479ef8050e4caec915a132b89edc7d805
I-am-a-Bird <krazydyo@gmail.com>
2025-05-08 00:49:06 +0300

    newBranch

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     cout<<"Hello GIT!"<<endl; 
     cout<<"ioy";
-    cout<<"we are making some changes..."
+    cout<<"we are making some changes...";
+    cout<<"newBranch";
     return 0;
 }
